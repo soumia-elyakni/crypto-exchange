efdefb668a6527b3bbffa057a0939f32e2739140
soumia elyakni <selyakni@gmail.com>
2022-10-24 00:19:40 +0100

    shaping file and sorting methode starting

--- crypto-usd/src/components/cryptoCurrencyTable.vue
+++ crypto-usd/src/components/cryptoCurrencyTable.vue
@@ -1,97 +1,131 @@
-<template>
-    <div>
+<template >
+    <div class="container mt-5 d-flex flex-column border-danger rounded-3">
 
-        <input
-        type="text"
-        class="form-control text-light bg-dark rounded-0 border-0 my-4"
-        placeholder="Search"
-        v-model="textSearch"
-        @keyup="searchCoin()"
-        autofocus
-      />
+        <input type="text" class="form-control text-light bg-dark rounded-0 border-0 my-4" placeholder="Search"
+            v-model="textSearch" @keyup="searchCoin()" autofocus />
 
         <table>
-        <thead>
-          <tr class="text-light bg-dark rounded-0 border-0 my-4">
-            <th v-for="(title, index) in titles" :key="index">
-              {{ title }}
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(coin, index) in filteredCoins" :key="coin.id">
-            <td class="text-muted">{{ index }}</td>
-            <td>
-              <img :src="coin.image" :alt="coin.name" style="width: 2rem" class="me-2" />
-              <span>
-                {{ coin.name }}
-              </span>
-              <span class="ms-2 text-muted text-uppercase">
-                {{ coin.symbol }}
-              </span>
-            </td>
-            <td>{{ coin.current_price.toLocaleString() }}</td>
-            <td
-              :class="[
-                coin.price_change_percentage_24h > 0
-                  ? 'text-success'
-                  : 'text-danger',
-              ]"
-            >
-              {{ coin.price_change_percentage_24h }}
-            </td>
-            <td>{{ coin.total_volume.toLocaleString() }}</td>
-          </tr>
-        </tbody>
-      </table>
+            <thead>
+                <tr class="text-light rounded-4 border-0 my-4">
+                    <th v-for="(title, index) in titles" :key="index">
+                        <button @click="sort(`${title}`)">
+                            {{ title }}
+                        </button>
+                        <hr class="w-100">
+                        <hr class="w-100">
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="( coin, index) in filteredCoins" :key="coin.id" class="mb-3">
+                    <td class="text-muted">{{ index +1 }}
+                        <hr class="w-100">
+                    </td>
+                    <td>
+                        <img :src="coin.image" :alt="coin.name" style="width: 1.2rem" class="me-2" />
+                        <span>
+                            {{ coin.name }}
+                        </span>
+                        <span class="ms-2 text-muted text-uppercase">
+                            {{ coin.symbol }}
+                        </span>
+                        <hr class="w-100">
+                    </td>
+                    <td>{{ coin.current_price.toLocaleString() }}
+                        <hr class="w-100">
+                    </td>
+                    <td :class="[
+                      coin.price_change_percentage_24h > 0
+                        ?'text-success'
+                        :'text-danger',
+                    ]">
+                        {{ coin.price_change_percentage_24h }}
+                        <hr class="w-100 text-light">
+                    </td>
+                    <td>{{ coin.total_volume.toLocaleString() }}
+                        <hr class="w-100">
+                    </td>
+
+                </tr>
+            </tbody>
+        </table>
+        <p class="text-light">
+        debug: sort={{currentSort}}
+        </p>
+
+        <p class="d-flex flex-row justify-content-evenly w-100 m-auto float-end text-white fw-bold pb-3">
+            <button @click="prevPage">Previous</button>
+            <button @click="nextPage">Next</button>
+        </p>
     </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      coins: [],
-      filteredCoins: [],
-      titles: ["#", "Coin", "Price", "Price Change", "24h Volume"],
-      textSearch: "",
-    };
-  },
-  async mounted() {
-    const res = await fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-    );
-    const data = await res.json();
-    this.coins = data;
-    this.filteredCoins = data;
-  },
-     methods: {
-       searchCoin() {
-         this.filteredCoins = this.coins.filter(
-           (coin) =>
-             coin.name.toLowerCase().includes(this.textSearch.toLowerCase()) ||
-             coin.symbol.toLowerCase().includes(this.textSearch.toLowerCase())
-         );
-       },
-     },
+    async mounted() {
+        const res = await fetch(
+            "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+        );
+        const data = await res.json();
+        this.coins = data;
+        this.filteredCoins = data;
+    },
+    data() {
+        return {
+            coins: [],
+            filteredCoins : this.filteredCoins,
+            titles: ["#", "Coin", "Price", "Price Change", "24h Volume"],
+            textSearch: "",
+        };
+    },
+    methods: {
+        searchCoin() {
+            this.filteredCoins = this.coins.filter(
+                (coin) =>
+                    coin.name.toLowerCase().includes(this.textSearch.toLowerCase()) ||
+                    coin.symbol.toLowerCase().includes(this.textSearch.toLowerCase())
+            );
+        },
+        sort:function(s) {
+      //if s == current sort, reverse
+       if(s === this.currentSort) {
+        this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
+      }
+      this.currentSort = s;
+    }
+        
+
+        // nextPage: function () {
+        //     if ((this.currentPage * this.pageSize) < this.coins.length) this.currentPage++;
+        // },
+        // prevPage: function () {
+        //     if (this.currentPage > 1) this.currentPage--;
+        // }
+    },
+    computed : {
+        filteredCoin: function() {
+     let filter = new RegExp(this.titles, 'i')
+     return this.filteredCoins.filter(el => el.title.match(filter))
+    }
+  }
+
 }
 
 
-  
+
+
+
 </script>
 
 <style lang="css">
-div{
+.container {
     margin: auto;
-    margin-top: 10px;
-    padding-top: 10px;
-    display: flex;
-    flex-direction: column;
     width: 90%;
     background-color: rgb(42, 41, 41);
-    border-radius: 10px;
+    border-radius: 5%;
 }
-table{
+
+table {
     width: 90%;
     margin: auto;
     border: none;
@@ -103,14 +137,11 @@ thead {
     width: 100%;
 }
 
-tbody{
+tbody {
     width: 100%;
 }
 
-tr{
+tr {
     text-align: left;
 }
-
-
-
 </style>
