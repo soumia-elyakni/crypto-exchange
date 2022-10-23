9d7b886f31ff83ca3dbf75617bd2b4f2a3f2c8e5
soumia elyakni <selyakni@gmail.com>
2022-10-23 19:57:12 +0100

    import bootstrap

--- crypto-usd/src/main.js
+++ crypto-usd/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 loadFonts()
 
