8769b6c96a9d43dc8a0aef01dcdf98ab138672d1
soumia elyakni <selyakni@gmail.com>
2022-10-23 02:21:44 +0100

    update cryptoCurrencyTable Component and install axios packages

--- crypto-usd/src/App.vue
+++ crypto-usd/src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CryptoCurrencyTable />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CryptoCurrencyTable from "./components/cryptoCurrencyTable.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "App",
+    components: {
+    CryptoCurrencyTable
+},
+  };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
